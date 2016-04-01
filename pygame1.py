e8e20329a4d25aa633a4f6cdec488249b6687769
Jesse Yang <j574y923@ku.edu>
2016-04-01 17:13:14 -0500

    Load images from images folder

--- pygame1.py
+++ pygame1.py
@@ -4,9 +4,48 @@
 
 #pipe image from http://vignette3.wikia.nocookie.net/fantendo/images/0/06/RocketPipes.png/revision/latest?cb=20100430132034
 
-import sys, pygame
+import sys, pygame, os
 from random import randint
 
+def load_images():
+    """Load all images required by the game and return a dict of them.
+
+    The returned dict has the following keys:
+    background: The game's background image.
+    bird-wingup: An image of the bird with its wing pointing upward.
+        Use this and bird-wingdown to create a flapping bird.
+    bird-wingdown: An image of the bird with its wing pointing downward.
+        Use this and bird-wingup to create a flapping bird.
+    pipe-end: An image of a pipe's end piece (the slightly wider bit).
+        Use this and pipe-body to make pipes.
+    pipe-body: An image of a slice of a pipe's body.  Use this and
+        pipe-body to make pipes.
+    """
+
+    def load_image(img_file_name):
+        """Return the loaded pygame image with the specified file name.
+
+        This function looks for images in the game's images folder
+        (./images/).  All images are converted before being returned to
+        speed up blitting.
+
+        Arguments:
+        img_file_name: The file name (including its extension, e.g.
+            '.png') of the required image, without a file path.
+        """
+        file_name = os.path.join('.', 'images', img_file_name)
+        img = pygame.image.load(file_name)
+        img.convert()
+        return img
+
+    return {'jayhawk': load_image('jayhawk.png'),
+            'background': load_image('repeatTest_smw.png'),
+            'pipe': load_image('pipe.png')
+            # images for animating the flapping bird -- animated GIFs are
+            # not supported in pygame
+            #'bird-wingup': load_image('bird_wing_up.png'),
+            #'bird-wingdown': load_image('bird_wing_down.png')
+            }
 
 def main():
     """The application's entry point.
@@ -22,16 +61,17 @@ def main():
 
     screen = pygame.display.set_mode(size)
 
+    images = load_images();
+
     #scrolling background declaration
-    bg = "repeatTest_smw.png"
-    back = pygame.image.load(bg).convert()
-    back2 = pygame.image.load(bg).convert()
-    back3 = pygame.image.load(bg).convert()
+    back = images['background']
+    back2 = images['background']
+    back3 = images['background']
     bgWidth, bgHeight = back.get_size()
     x = 0
     bgdelay = 0
 
-    jayhawk = pygame.image.load("jayhawk.png")
+    jayhawk = images['jayhawk']
     jayhawk = pygame.transform.scale(jayhawk, (50, 50))
     jayrect = jayhawk.get_rect()
 
