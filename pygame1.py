468623b8002f74329e1b8a656fe2319d555b35d0
j574y923 <j574y923@ku.edu>
2016-03-31 22:34:25 -0500

    Add code for scrolling background

--- pygame1.py
+++ pygame1.py
@@ -11,6 +11,14 @@ black = 0,0,0
 
 screen = pygame.display.set_mode(size)
 
+#scrolling background declaration
+bg = "repeatTest_smw.png"
+back = pygame.image.load(bg).convert()
+back2 = pygame.image.load(bg).convert()
+back3 = pygame.image.load(bg).convert()
+bgWidth, bgHeight = back.get_size()
+x = 0
+
 jayhawk = pygame.image.load("jayhawk.png")
 jayhawk = pygame.transform.scale(jayhawk, (50, 50))
 jayrect = jayhawk.get_rect()
@@ -30,8 +38,18 @@ while 1:
             if event.key == pygame.K_DOWN:
                 jayrect = jayrect.move(0, 20)
                 
-    screen.fill(black)
+    screen.fill((255, 231, 181))
+
+    #draw background
+    screen.blit(back, (x,height - bgHeight))
+    screen.blit(back2,(x + bgWidth,height - bgHeight))
+    screen.blit(back3,(x + bgWidth + bgWidth,height - bgHeight))
+    #make background scroll
+    x = x - 1
+    if x == 0 - bgWidth:
+        x = 0
+        
     screen.blit(jayhawk, jayrect)
     pygame.display.update()
     pygame.display.flip()
-    pygame.time.delay(100)
+    pygame.time.delay(10)
