ec99ab111a490fae4413c233a67a41cae32e3f66
Victor Luiz da Silva <victorluiz.m4@gmail.com>
2016-04-01 18:27:55 -0500

    Add up/down movement functionality

--- pygame1.py
+++ pygame1.py
@@ -1,7 +1,3 @@
-
-########
-# MOVE JAYHAWK WITH ARROWS, PRESS SPACEBAR TO QUIT
-
 import sys, pygame
 
 pygame.init()
@@ -10,28 +6,38 @@ size = width, height = 600, 500
 black = 0,0,0
 
 screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Flappy Jayhawk")
+
+isGoingUp = False;
+movementCounter = 0;
 
 jayhawk = pygame.image.load("jayhawk.png")
 jayhawk = pygame.transform.scale(jayhawk, (50, 50))
 jayrect = jayhawk.get_rect()
+jayrect = jayrect.move(80, 200);
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_SPACE:   # listens for pressing spacebar
-                pygame.quit()       # closes program and window
-                sys.exit()
-            if event.key == pygame.K_LEFT:
-                jayrect = jayrect.move(-20,0)
-            if event.key == pygame.K_RIGHT:
-                jayrect = jayrect.move(20,0)
-            if event.key == pygame.K_UP:
-                jayrect = jayrect.move(0, -20)
-            if event.key == pygame.K_DOWN:
-                jayrect = jayrect.move(0, 20)
+            if event.key == pygame.K_ESCAPE:   # listens for ESC key being pressed
+                pygame.quit()       # closes program 
+                sys.exit()          # closes window
+            if event.key == pygame.K_SPACE:
+                isGoingUp = True;                    
+                
+    if(not(isGoingUp)):                     # bird moves down
+       jayrect = jayrect.move(0, 20)
+
+    if(isGoingUp):
+        jayrect = jayrect.move(0, -20)
+        movementCounter = movementCounter + 1
+        if(movementCounter > 7):
+            isGoingUp = False;
+            movementCounter = 0;
+
                 
     screen.fill(black)
     screen.blit(jayhawk, jayrect)
     pygame.display.update()
     pygame.display.flip()
-    pygame.time.delay(100)
+    pygame.time.delay(60)
