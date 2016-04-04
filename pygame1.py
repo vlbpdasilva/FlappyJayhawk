2a578309f6d73715d6d5ccd0287470fb9c16bf64
j574y923 <j574y923@ku.edu>
2016-04-04 17:40:35 -0500

    Resize Jayhawk, add pipe

--- pygame1.py
+++ pygame1.py
@@ -1,4 +1,3 @@
-
 ########
 # MOVE JAYHAWK WITH ARROWS, PRESS SPACEBAR TO QUIT
 
@@ -9,20 +8,17 @@ from random import randint
 
 class Jayhawk(pygame.sprite.Sprite):
     """The Jayhawk that the player will be controlling.
-
     The Jayhawk will ascend or descend and its main objective is to avoid
     colliding with pipes. Ascending will occur when the player hits up/space.
     Descending will occur when the player is not causing the Jayhawk to
     ascend. Colliding with pipes will cause the Jayhawk to lose health.
     At 0 health, the player loses the game.
-
     Attributes: (NOTE: THIS WILL BE CHANGED. these are not the actual attributes or constants,
                         they are only here as an example of documentation for now)
     x: The bird's X coordinate.
     y: The bird's Y coordinate.
     msec_to_climb: The number of milliseconds left to climb, where a
         complete climb lasts Bird.CLIMB_DURATION milliseconds.
-
     Constants:
     WIDTH: The width, in pixels, of the bird's image.
     HEIGHT: The height, in pixels, of the bird's image.
@@ -37,7 +33,6 @@ class Jayhawk(pygame.sprite.Sprite):
     
     def __init__(self, x, y, scale, image):
         """Initialise a new Jayhawk instance.
-
         Arguments:
         x: The Jayhawk's initial X coordinate.
         y: The Jayhawk's initial Y coordinate.
@@ -47,13 +42,12 @@ class Jayhawk(pygame.sprite.Sprite):
         self.x = x
         self.y = y
         self.Jayhawk_image = image
-        self.scale = pygame.transform.scale(self.Jayhawk_image, scale)
+        self.Jayhawk_image = pygame.transform.scale(self.Jayhawk_image, scale)
         self.Jayhawk_mask = pygame.mask.from_surface(self.Jayhawk_image)
 
     @property
     def image(self):
         """Get a Surface containing this bird's image.
-
         This will decide whether to return an image where the bird's
         visible wing is pointing upward or where it is pointing downward
         based on pygame.time.get_ticks().  This will animate the flapping
@@ -64,20 +58,111 @@ class Jayhawk(pygame.sprite.Sprite):
     @property
     def mask(self):
         """Get a bitmask for use in collision detection.
-
         The bitmask excludes all pixels in self.image with a
         transparency greater than 127."""
         return self.Jayhawk_mask
 
     @property
     def rect(self):
-        """Get the bird's position, width, and height, as a pygame.Rect."""
-        return Jayhawk_rect(self.x, self.y, 50, 50)
+        """Get the bird's position, width, and height, as a pygame.Rect.
+
+            THE WIDTH AND HEIGHT PARAMETERS DON'T WORK?"""
+        return pygame.Rect(self.x, self.y, 25, 25)
+
+
+class Pipe(pygame.sprite.Sprite):
+    """
+    x: The bird's X coordinate.
+    y: The bird's Y coordinate.
+    msec_to_climb: The number of milliseconds left to climb, where a
+        complete climb lasts Bird.CLIMB_DURATION milliseconds.
+    Constants:
+    WIDTH: The width, in pixels, of the bird's image.
+    HEIGHT: The height, in pixels, of the bird's image.
+    SINK_SPEED: With which speed, in pixels per millisecond, the bird
+        descends in one second while not climbing.
+    CLIMB_SPEED: With which speed, in pixels per millisecond, the bird
+        ascends in one second while climbing, on average.  See also the
+        Bird.update docstring.
+    CLIMB_DURATION: The number of milliseconds it takes the bird to
+        execute a complete climb.
+    """
     
+    def __init__(self, image, game_window_width):
+        """Initialise a new Jayhawk instance.
+        Arguments:
+        x: The Jayhawk's initial X coordinate.
+        y: The Jayhawk's initial Y coordinate.
+        scale: The Jayhawk's size multiplier.
+        """
+        super(Pipe, self).__init__()
+        self.x = game_window_width
+        self.reset_x = game_window_width
+        self.y = 25
+
+        self.Pipe_image_top = image
+        self.Pipe_image_top = pygame.transform.rotate(self.Pipe_image_top, 180)
+        self.Pipe_mask_top = pygame.mask.from_surface(self.Pipe_image_top)
+        
+        self.Pipe_image_bot = image
+        self.Pipe_mask_bot = pygame.mask.from_surface(self.Pipe_image_bot)
+
+    def scroll(self):
+        self.x = self.x - 1
+        if(self.x + 50 == 0):
+            self.x = self.reset_x
+            self.y = randint(25, 375)
+    
+    @property
+    def image_top(self):
+        """Get a Surface containing this bird's image.
+        This will decide whether to return an image where the bird's
+        visible wing is pointing upward or where it is pointing downward
+        based on pygame.time.get_ticks().  This will animate the flapping
+        bird, even though pygame doesn't support animated GIFs.
+        """
+        return self.Pipe_image_top
+
+    @property
+    def mask_top(self):
+        """Get a bitmask for use in collision detection.
+        The bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
+        return self.Pipe_mask_top
+
+    @property
+    def rect_top(self):
+        """Get the bird's position, width, and height, as a pygame.Rect.
+
+            THE WIDTH AND HEIGHT PARAMETERS DON'T WORK?"""
+        return pygame.Rect(self.x, self.y - 504, 25, 25)#pipe's img height is 504
+
+    @property
+    def image_bot(self):
+        """Get a Surface containing this bird's image.
+        This will decide whether to return an image where the bird's
+        visible wing is pointing upward or where it is pointing downward
+        based on pygame.time.get_ticks().  This will animate the flapping
+        bird, even though pygame doesn't support animated GIFs.
+        """
+        return self.Pipe_image_bot
+
+    @property
+    def mask_bot(self):
+        """Get a bitmask for use in collision detection.
+        The bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
+        return self.Pipe_mask_bot
+
+    @property
+    def rect_bot(self):
+        """Get the bird's position, width, and height, as a pygame.Rect.
+
+            THE WIDTH AND HEIGHT PARAMETERS DON'T WORK?"""
+        return pygame.Rect(self.x, self.y + 100, 25, 25)
         
 def load_images():
     """Load all images required by the game and return a dict of them.
-
     The returned dict has the following keys:
     background: The game's background image.
     bird-wingup: An image of the bird with its wing pointing upward.
@@ -92,11 +177,9 @@ def load_images():
 
     def load_image(img_file_name):
         """Return the loaded pygame image with the specified file name.
-
         This function looks for images in the game's images folder
         (./images/).  All images are converted before being returned to
         speed up blitting.
-
         Arguments:
         img_file_name: The file name (including its extension, e.g.
             '.png') of the required image, without a file path.
@@ -117,7 +200,6 @@ def load_images():
 
 def main():
     """The application's entry point.
-
     If someone executes this module (instead of importing it, for
     example), this function is called.
     """
@@ -139,9 +221,12 @@ def main():
     x = 0
     bgdelay = 0
 
+    #scrolling pipe declaration
+    pipe = Pipe(images['pipe'], width)
+    #piperect = pipe.rect #this is updated by calling scroll and then calling pipe's rect property
+
     jayhawk = Jayhawk(0,0,(50,50), images['jayhawk'])
-    #jayhawk = pygame.transform.scale(jayhawk, (50, 50))
-    jayrect = images['jayhawk'].get_rect()
+    jayrect = jayhawk.rect
 
     while 1:
         for event in pygame.event.get():
@@ -151,11 +236,11 @@ def main():
                     sys.exit()
                 if event.key == pygame.K_LEFT:
                     jayrect = jayrect.move(-20,0)
-                if event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT:
                     jayrect = jayrect.move(20,0)
-                if event.key == pygame.K_UP:
+                elif event.key == pygame.K_UP:
                     jayrect = jayrect.move(0, -20)
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN:
                     jayrect = jayrect.move(0, 20)
                     
         screen.fill((255, 231, 181))
@@ -170,6 +255,12 @@ def main():
             x = x - 1
             if x == 0 - bgWidth:
                 x = 0
+
+        #draw pipe
+        screen.blit(pipe.image_top, pipe.rect_top)
+        screen.blit(pipe.image_bot, pipe.rect_bot)
+        #make pipe scroll
+        pipe.scroll()
         
         screen.blit(jayhawk.image, jayrect)
         pygame.display.update()
