2519fb37ff24b0f46a09de991625dc82cb4a90e8
Victor Luiz da Silva <victorluiz.m4@gmail.com>
2016-04-08 17:53:26 -0500

    Refactor into separate files. Improve Pydoc

---
+++ Jayhawk.py
@@ -0,0 +1,58 @@
+class Jayhawk(pygame.sprite.Sprite):
+    """The Jayhawk that the player will be controlling.
+    The Jayhawk will ascend or descend and its main objective is to avoid
+    colliding with pipes. Ascending will occur when the player hits up/space.
+    Descending will occur when the player is not causing the Jayhawk to
+    ascend. Colliding with pipes will cause the Jayhawk to lose health.
+    At 0 health, the player loses the game.
+    Attributes: (NOTE: THIS WILL BE CHANGED. these are not the actual attributes or constants,
+                        they are only here as an example of documentation for now)
+    x: The bird's X coordinate.
+    y: The bird's Y coordinate.
+    msec_to_climb: The number of milliseconds left to climb, where a
+        complete climb lasts Bird.CLIMB_DURATION milliseconds.
+    Constants:
+    WIDTH: The width, in pixels, of the bird's image.
+    HEIGHT: The height, in pixels, of the bird's image.
+    SINK_SPEED: With which speed, in pixels per millisecond, the bird
+        descends in one second while not climbing.
+    CLIMB_SPEED: With which speed, in pixels per millisecond, the bird
+        ascends in one second while climbing, on average.  See also the
+        Bird.update docstring.
+    CLIMB_DURATION: The number of milliseconds it takes the bird to
+        execute a complete climb.
+    """
+    
+    def __init__(self, x, y, scale, image):
+        """Initialise a new Jayhawk instance.
+        Arguments:
+        x: The Jayhawk's initial X coordinate.
+        y: The Jayhawk's initial Y coordinate.
+        scale: The Jayhawk's size multiplier.
+        image: The Jayhawk's image.
+        """
+        super(Jayhawk, self).__init__()
+        self.x = x
+        self.y = y
+        self.Jayhawk_image = image
+        self.Jayhawk_image = pygame.transform.scale(self.Jayhawk_image, scale)
+        self.Jayhawk_mask = pygame.mask.from_surface(self.Jayhawk_image)
+
+    @property
+    def image(self):
+        """Get a Surface containing this bird's image.
+        """
+        return self.Jayhawk_image
+
+    @property
+    def mask(self):
+        """Get a bitmask for use in collision detection.
+        The bitmask excludes all pixels in self.image with a
+        transparency greater than 127."""
+        return self.Jayhawk_mask
+
+    @property
+    def rect(self):
+        """Get the bird's position, width, and height, as a pygame.Rect.
+            THE WIDTH AND HEIGHT PARAMETERS DON'T WORK?"""
+        return pygame.Rect(self.x, self.y, 25, 25)
